901efba946e1fa9ead986cffebcd279ff14610f0
nguyenluatx-rgb <nguyenluatx@gmail.com>
2025-10-05 07:21:01 +0000

    update game with list

--- part1_array_list.py
+++ part1_array_list.py
@@ -45,3 +45,61 @@ print(arr)
 # chuong trinh sap xep mang giam dan
 arr.sort(reverse=True)
 print(arr)
+
+
+# Tao mot list random
+import random
+random_list = random.sample(range(1, 100), 10)
+print(random_list)
+
+random_list.sort()
+print(random_list)
+random_list.sort(reverse=True)
+print(random_list)
+
+# BAI TAP: Tao mot game don gian
+
+user = []
+admin = []
+
+def calculate(list):
+    total = 0
+    for x in list:
+        total += x
+    return total
+
+def init_game():
+    for i in range(2):
+        user.append(random.randint(1, 10))
+        admin.append(random.randint(1, 10))
+    print(f"User: {user}, total: {calculate(user)}")
+    print(f"Admin: {admin}, total: {calculate(admin)}")
+
+def game_turn(user, admin):
+    # For user
+    while (calculate(user) < 10):
+        user.append(random.randint(1, 10))
+        print(f"User: {user}, total: {calculate(user)}")
+    
+    # For admin
+    while (calculate(admin) < 10):
+        admin.append(random.randint(1, 10))
+        print(f"Admin: {admin}, total: {calculate(admin)}")
+    
+    # Check winner, if total > 15 -> lose, esle check total
+    user_total = calculate(user)
+    admin_total = calculate(admin)
+    if user_total > 15:
+        print("User lose")
+    elif admin_total > 15:
+        print("Admin lose")
+    else:
+        if user_total > admin_total:
+            print("User win")
+        elif user_total < admin_total:
+            print("Admin win")
+        else:
+            print("Draw")
+
+init_game()
+game_turn(user, admin)
